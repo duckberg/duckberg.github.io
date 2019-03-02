2e3bb1fb60b8f0aca41a20ecbe9e24eab7383639
Lalit Tanwar <lalit.tanwar07@gmail.com>
2019-03-02 19:50:45 +0000

    build.sh created online with Bitbucket

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# By default we use the Node.js version set in your package.json or the latest
+# version from the 0.10 release
+#
+# You can use nvm to install any Node.js (or io.js) version you require.
+# nvm install 4.0
+
+#npm update
+
+#grunt build
+
+lftp -c "open -u $FTP_USER,$FTP_PASSWORD $FTP_SERVER; set ssl:verify-certificate no; put -O / .htaccess"
+lftp -c "open -u $FTP_USER,$FTP_PASSWORD $FTP_SERVER; set ssl:verify-certificate no; put -O / index.html"
+lftp -c "open -u $FTP_USER,$FTP_PASSWORD $FTP_SERVER; set ssl:verify-certificate no; put -O / business.html"
+
+lftp -c "open -u $FTP_USER,$FTP_PASSWORD $FTP_SERVER; set ssl:verify-certificate no; mirror -R --parallel=4 --exclude-glob .git js/ /js"
+lftp -c "open -u $FTP_USER,$FTP_PASSWORD $FTP_SERVER; set ssl:verify-certificate no; mirror -R --parallel=4 --exclude-glob .git jsmin/ /jsmin"
+lftp -c "open -u $FTP_USER,$FTP_PASSWORD $FTP_SERVER; set ssl:verify-certificate no; mirror -R --parallel=4 --exclude-glob .git style/ /style"
+lftp -c "open -u $FTP_USER,$FTP_PASSWORD $FTP_SERVER; set ssl:verify-certificate no; mirror -R --parallel=4 --exclude-glob .git img-src/ /img-src"
+lftp -c "open -u $FTP_USER,$FTP_PASSWORD $FTP_SERVER; set ssl:verify-certificate no; mirror -R --parallel=4 --exclude-glob .git img/ /img"
+
+
+lftp -c "open -u $FTP_USER,$FTP_PASSWORD $FTP_SERVER; set ssl:verify-certificate no; mirror -R no/ /no"
